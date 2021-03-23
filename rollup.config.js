ac715a271141c75b9ae1fcf5ab0e0a92d1c0760b
hirorock <hirorock@gmail.com>
2021-03-23 13:27:12 +0900

    fix: rm rollup

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  input: "src/cli.js",
-  output: {
-    file: "dist/bundle.js",
-    format: "cjs",
-    compact: true,
-  },
-  external: ["node-fetch", "universal-analytics", "chalk", "boxen", "uuid"],
-};
