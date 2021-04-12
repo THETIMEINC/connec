260cefe42e183a394774839069a64b2d20d5b36a
hirorock <hirorock@gmail.com>
2021-04-12 14:27:40 +0900

    fix: username regex

--- src/get-username.js
+++ src/get-username.js
@@ -2,8 +2,9 @@ const myArgs = process.argv.slice(2)
 
 module.exports = () => {
   // NOTE: regex similar to twitter.
-  this.twitValid = /^@.(\w){1,15}$/
+  this.twitValid = /^(@?)(\w){1,15}$/
   this.userparam = myArgs[0]
+  const target = this.userparam.match(this.twitValid)
 
-  return this.twitValid.test(this.userparam) ? this.userparam : ''
+  return target ? (target[1] ? target[0] : `@${target[0]}`) : ''
 }
