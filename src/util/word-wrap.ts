91608ea9d0d40a0440bedb75adf90cb000a83f5c
hirorock <hirorock@gmail.com>
2021-05-11 23:33:41 +0900

    feat: flexible text length

--- src/util/word-wrap.ts
+++ src/util/word-wrap.ts
@@ -1,5 +1,14 @@
+// TODO: Stop `jp-wrap`. Someday, I'll support prohibited characters & emoji.
 const jpWrap = require('jp-wrap')
-const jWrap = new jpWrap(60)
+const termSize = require('term-size')
+
+// NOTE: set card size. (= terminal size)
+const wrapSize = 60
+const wrapMargin = 10
+const { columns } = termSize()
+const wrapSetSize = Math.min(wrapSize, columns - wrapMargin)
+
+const jWrap = new jpWrap(wrapSetSize)
 
 export function wordWrap(text: string): string {
   return jWrap(text)
